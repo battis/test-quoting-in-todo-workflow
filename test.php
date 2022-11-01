7fae277be8251b85b2d07c05ff430e14aad1b00b
Seth Battis <seth@battis.net>
2022-11-01 09:15:47 -0400

    Test with single-quotes

--- test.php
+++ test.php
@@ -3,3 +3,8 @@
 // TODO A todo item (config with double quotes)
 
 // FIXME A fixme item (config with double quotes)
+
+// TODO Another todo item (config with single quotes)
+
+// FIXME Another fixme item (config with single quotes)
+
